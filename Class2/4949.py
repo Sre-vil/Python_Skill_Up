3c6cba107be1e728a037be0db42776fdf0eb7c4c
Srevil <defi622@naver.com>
2025-05-26 14:21:55 +0900

    Updated

--- Class2/4949.py
+++ Class2/4949.py
@@ -1,11 +1,24 @@
-res = []
-
 while True:
     tmp = input()
-    if tmp=='.': break
+    res = []
     
-    if tmp.count('(') == tmp.count(')') and tmp.count('[') == tmp.count(']'):
-        res.append('yes')
-    else: res.append('no')
-
-print(*res)
+    if tmp == '.': break
+    
+    for i in tmp:
+        if i == '(' or i == '[':
+            res.append(i)
+        elif i == ']':
+            if len(res) != 0 and res[-1] == '[':
+                res.pop()
+            else:
+                res.append(i)
+                break
+        elif i == ')':
+            if len(res) != 0 and res[-1] == '(':
+                res.pop()
+            else:
+                res.append(i)
+                break
+    
+    if len(res) == 0: print("yes")
+    else: print("no")
